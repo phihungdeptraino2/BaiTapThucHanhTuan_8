f4b69b419d3e86eabd01fa5f3200a126cfd1d80e
phihungdeptraino2 <hungdz1372004@gmail.com>
2025-04-12 07:04:42 +0700

    Cau1

---
+++ myapp/src/components/DetailedReport.jsx
@@ -0,0 +1,10 @@
+import avatar from "../assets/avatar.png";
+import { Pencil } from "lucide-react";
+
+import { useEffect, useState } from "react";
+const DetailedReport = () => {
+  const [data, setData] = useState([]);
+
+  return <div className="table-container"></div>;
+};
+export default DetailedReport;
---
+++ myapp/src/components/OverView.jsx
@@ -0,0 +1,6 @@
+// src/components/OverView.jsx
+import { useEffect, useState } from "react";
+
+export default function OverView() {
+  return <div className="overview-container"></div>;
+}
