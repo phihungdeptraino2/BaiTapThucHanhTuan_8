089377c48ff9bdd21f84f0274b0f8a6d080240a8
phihungdeptraino2 <hungdz1372004@gmail.com>
2025-04-12 14:09:18 +0700

    Cau4

--- myapp/src/components/DetailedReport.jsx
+++ myapp/src/components/DetailedReport.jsx
@@ -1,49 +1,67 @@
-import React from "react";
+import avatar1 from "../assets/avatar1.png";
+import { Pencil } from "lucide-react";
 import DataTable from "react-data-table-component";
+import { useEffect, useState } from "react";
+import "../styles/DetailedReportStyle.css";
 
 const columns = [
   {
+    name: "",
+    cell: () => <input type="checkbox" />,
+    width: "60px",
+  },
+  {
     name: "CUSTOMER NAME",
     selector: (row) => row.name,
+    cell: (row) => (
+      <div className="customer-info">
+        <img src={avatar1} alt={avatar1} className="avatar" />
+        <span className="customer-name">{row.name}</span>
+      </div>
+    ),
+    sortable: true,
   },
   {
     name: "COMPANY",
     selector: (row) => row.company,
+    sortable: true,
   },
   {
     name: "ORDER VALUE",
     selector: (row) => row.value,
+    sortable: true,
   },
   {
     name: "ORDER DATE",
     selector: (row) => row.date,
+    sortable: true,
   },
   {
     name: "STATUS",
     selector: (row) => row.status,
-  },
-];
-const data = [
-  {
-    name: "John Doe",
-    company: "ABC Corp",
-    value: "$500",
-    date: "2025-04-12",
-    status: "Completed",
+    cell: (row) => (
+      <span
+        className={`status-badge ${row.status.toLowerCase().replace(" ", "-")}`}
+      >
+        {row.status}
+      </span>
+    ),
+    sortable: true,
   },
 ];
 
 const DetailedReport = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3001/report")
+      .then((res) => res.json())
+      .then((data) => setData(data))
+      .catch((err) => console.error("Fetch error:", err));
+  }, []);
   return (
-    <div>
-      <DataTable
-        columns={columns}
-        data={data}
-        pagination
-        noDataComponent="No Data Available"
-      />
+    <div className="table-wrapper">
+      <DataTable columns={columns} data={data} pagination highlightOnHover />
     </div>
   );
 };
-
 export default DetailedReport;
