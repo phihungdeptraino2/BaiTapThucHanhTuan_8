b68b1d89fc653577f4c4072d1414c79bd9e9a2a7
phihungdeptraino2 <hungdz1372004@gmail.com>
2025-04-12 21:25:45 +0700

    Cau9

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -6,15 +6,35 @@ import Sidebar from "./components/Sidebar";
 import Header from "./components/Header";
 import OverView from "./components/OverView.jsx";
 import DetailedReport from "./components/DetailedReport.jsx";
-import { Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import Project from "./components/Project.jsx";
+import DashBoard from "./components/DashBoard.jsx";
+import Teams from "./components/Teams.jsx";
+import Analytics from "./components/Analytics.jsx";
+import Messages from "./components/Messages.jsx";
+import Integrations from "./components/Integrations.jsx";
 function App() {
   return (
     <div className="grid-container">
       <Sidebar />
       <main className="main-content">
         <Header />
-        <OverView />
-        <DetailedReport />
+        {/* <OverView />
+        <DetailedReport /> */}
+
+        <Routes>
+          <Route>
+            <Route path="/" element={<DashBoard></DashBoard>} />
+            <Route path="/projects" element={<Project></Project>} />
+            <Route path="/teams" element={<Teams></Teams>} />
+            <Route path="/messages" element={<Messages></Messages>} />
+            <Route path="/analytics" element={<Analytics></Analytics>} />
+            <Route
+              path="/integrations"
+              element={<Integrations> </Integrations>}
+            />
+          </Route>
+        </Routes>
       </main>
     </div>
   );
